bfac127fe1b0d24f16db7ead509a33759c8eb4ac
Petter Padilha <petterson_sorriso@hotmail.com>
2023-01-26 22:46:56 -0400

    Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import IncomeImg from '../../assets/income.svg';
+import OutcomeImg from '../../assets/outcome.svg';
+import TotalImg from '../../assets/total.svg';
+import { Container } from "./styles";
+
+
+export function Summary() {
+    return(
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={IncomeImg} alt="Entrada" />
+                </header>
+                <strong>R$ 1.000,00</strong>
+            </div>
+
+            <div>
+                <header>
+                    <p>Saidas</p>
+                    <img src={OutcomeImg} alt="Saidas" />
+                </header>
+                <strong>R$ - 350,00</strong>
+            </div>
+
+            <div className="hightlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={TotalImg} alt="Total" />
+                </header>
+                <strong>R$ 750,00</strong>
+            </div>
+        </Container>
+    );
+}
