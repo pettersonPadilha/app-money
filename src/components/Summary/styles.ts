7a47a2ba8cbaa54f2f16126a4703ff8329c6d016
Petter Padilha <petterson_sorriso@hotmail.com>
2023-01-26 22:47:04 -0400

    Estilo Sumary

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3,1fr);
+    gap: 2rem;
+    margin-top: -10rem;
+
+    div {
+        background: var(--shape);
+        padding: 1.5rem 2rem;
+        border-radius: 0.25rem;
+        color: var(--text-title);
+
+        header {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+        strong {
+            display: block;
+            margin-top: 1rem;
+            font-size: 2rem;
+            font-weight: normal;
+            line-height: 3rem;
+        }
+        &.hightlight-background {
+            background: #33cc95;
+            color: white
+        }
+    }
+    @media (max-width: 528px) {
+        grid-template-columns: repeat(1,1fr);
+  }
+ 
+`
+
