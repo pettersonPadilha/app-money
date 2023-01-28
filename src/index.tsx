7678d762cb97d6ea8a20c9d7fcd4b830be5b7be6
Petter Padilha <petterson_sorriso@hotmail.com>
2023-01-28 11:42:51 -0400

    Formatando valores

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,31 @@ import { App } from './App';
 createServer({
 
   models: {
-    transactions: Model,
+    transaction: Model,
+  },
+
+  //Dados pre-cadastrado
+  seeds(server) {
+    server.db.loadData({
+      transactions : [
+        {
+          id: 1,
+          title: 'freenlace de website',
+          type: 'deposit',
+          category: 'dev',
+          amount: 3500,
+          createAt: new Date('2022-05-10 10:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'casa',
+          amount: 1500,
+          createAt: new Date('2022-05-10 15:00:00'),
+        },
+      ],
+    })
   },
 
   routes() {
