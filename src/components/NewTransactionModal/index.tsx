988e2ce8741015a2f78b430dc4aa241fea44ac32
Petter Padilha <petterson_sorriso@hotmail.com>
2023-01-27 23:14:26 -0400

    Input modal

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,25 @@
+import Modal from 'react-modal'
+import { Container } from './styles';
+
+interface NewTransactionsModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+
+}
+
+export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionsModalProps) {
+    return(
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose}  overlayClassName="react-modal-overlay" className="react-modal-content">
+
+        <Container>
+            <h2>Cadastrar Transação</h2>
+            <input placeholder='Titulo' />
+            <input type="number"placeholder='Valor' />
+            <input placeholder='Categoria' />
+
+            <button type='submit'>Cadastrar</button>
+        </Container>
+
+    </Modal>
+    );
+}
